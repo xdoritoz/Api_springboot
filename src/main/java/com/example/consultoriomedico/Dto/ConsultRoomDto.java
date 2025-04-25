aaef26bf124d36bab8348b4a84c1a63160b98c72
xdoritoz <123668559+xdoritoz@users.noreply.github.com>
2025-04-25 16:50:11 -0500

    creating repositorys - creating DTO

---
+++ src/main/java/com/example/consultoriomedico/Dto/ConsultRoomDto.java
@@ -0,0 +1,10 @@
+package com.example.consultoriomedico.Dto;
+
+public class ConsultRoomDto{
+
+    private Long id;
+    private String name;
+    private String floor;
+    private String description;
+    
+}
